5b1f90f739ac4968e47c3192eaf4287b7f5b4ebc
Nigel Dixon <nigel@iamdixon.net>
2015-10-14 14:35:02 +0100

    FIXED! removing temp folder too early

--- SVGtoICO.bat
+++ SVGtoICO.bat
@@ -28,5 +28,5 @@ FOR /r %%i in (*.svg) DO (
 		inkscape -z -e temp/%%~ni-%%s.png -w %%s -h %%s %%i
 	)
 	convert temp/%%~ni-16.png temp/%%~ni-24.png temp/%%~ni-32.png temp/%%~ni-48.png temp/%%~ni-64.png %%~ni.ico
-	RMDIR temp /S /Q
 )
+RMDIR temp /S /Q
